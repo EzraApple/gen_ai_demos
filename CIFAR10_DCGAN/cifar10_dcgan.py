a249415719eff2858bad2fd77ab6a26e9ff24bbe
Ezra Apple <ezraapple@berkeley.edu>
2023-09-19 12:58:35 -0700

    demo for cifar10 dcgan

--- CIFAR10_DCGAN/cifar10_dcgan.py
+++ CIFAR10_DCGAN/cifar10_dcgan.py
@@ -10,11 +10,14 @@ class Generator(nn.Module):
     def __init__(self, noise_channels, image_channels, features):
         super().__init__()
         self.generate = nn.Sequential(
-            nn.ConvTranspose2d(noise_channels, features, 4, 2, 0),
+            nn.ConvTranspose2d(noise_channels, 4 * features, 4, 2, 0),
+            nn.BatchNorm2d(4 * features),
             nn.ReLU(),
-            nn.ConvTranspose2d(features, features, 4, 2, 1),
+            nn.ConvTranspose2d(4 * features, 2 * features, 4, 2, 1),
+            nn.BatchNorm2d(2 * features),
             nn.ReLU(),
-            nn.ConvTranspose2d(features, features, 4, 2, 1),
+            nn.ConvTranspose2d(2 * features, features, 4, 2, 1),
+            nn.BatchNorm2d(features),
             nn.ReLU(),
             nn.ConvTranspose2d(features, image_channels, 4, 2, 1),
             nn.Tanh()
@@ -32,14 +35,18 @@ class Discriminator(nn.Module):
         super().__init__()
         self.discriminate = nn.Sequential(
             nn.Conv2d(image_channels, features, 4, 2, 1),
+            nn.BatchNorm2d(features),
             nn.ReLU(),
-            nn.Conv2d(features, features, 4, 2, 1),
+            nn.Conv2d(features, 2 * features, 4, 2, 1),
+            nn.BatchNorm2d(2 * features),
             nn.ReLU(),
-            nn.Conv2d(features, features, 4, 2, 1),
+            nn.Conv2d(2 * features, 4 * features, 4, 2, 1),
+            nn.BatchNorm2d(4 * features),
             nn.ReLU(),
-            nn.Conv2d(features, features, 4, 2, 1),
+            nn.Conv2d(4 * features, 8 * features, 4, 2, 1),
+            nn.BatchNorm2d(8 * features),
             nn.ReLU(),
-            nn.Conv2d(features, 1, 4, 2, 1),
+            nn.Conv2d(8 * features, 1, 4, 2, 1),
             nn.Sigmoid()
         )
 
